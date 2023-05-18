ad93d2e86787650b303d55b7223cb70ea92c2409
Adarsh <adarshkumar7478@gmail.com>
2023-05-18 07:38:04 +0530

    broken homepage to components and then added some css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import HomePage from "./homepage.component";
-import "./homepage.style.scss";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.style.scss";
 function App() {
   return (
     <div>
